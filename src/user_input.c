5276ff1212ddeb192ac093ff3816e0e1a3e4864e
nour zamel <nourzamel35@gmail.com>
2023-10-08 22:44:49 +0300

    added improved user input verification

--- src/user_input.c
+++ src/user_input.c
@@ -1,11 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "user_input.h"
 
 void getPlayerNames(char player1[], char player2[]) {
-    printf("Enter the name of Player 1: ");
-    scanf("%s", player1);
-    printf("Enter the name of Player 2: ");
-    scanf("%s", player2);
+    while (1) {
+        printf("Enter the name of Player 1 (single word, no spaces): ");
+        scanf("%s", player1);
+        printf("Enter the name of Player 2 (single word, no spaces): ");
+        scanf("%s", player2);
+
+        if (strcmp(player1, player2) == 0) {
+            printf("Names cannot be the same. Try different names.\n");
+        } else if (strchr(player1, ' ') || strchr(player2, ' ')) {
+            printf("Names must be single words with no spaces. Try again.\n");
+        } else {
+            break;
+        }
+    }
 }
 
 void getPlayerSpell(int currentPlayer, char spell[], char player1[], char player2[]) {
