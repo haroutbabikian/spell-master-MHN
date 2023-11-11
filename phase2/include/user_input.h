2931b894a17eb214be3b6603ca3d71ee2c066dbf
haroutbabikian <hhbabikian@gmail.com>
2023-11-11 19:51:33 +0200

    Initialize phase 2 folder

---
+++ phase2/include/user_input.h
@@ -0,0 +1,10 @@
+#ifndef USER_INPUT_H
+#define USER_INPUT_H
+
+#define MAX_NAME_LENGTH 50
+
+void getPlayerNames(char player1[], char player2[]);
+
+void getPlayerSpell(int currentPlayer, char spell[], char player1[], char player2[]);
+
+#endif
---
+++ phase2/include/winner_determination.h
@@ -0,0 +1,6 @@
+#ifndef WINNER_DETERMINATION_H
+#define WINNER_DETERMINATION_H
+
+void announceWinner(char player1[], char player2[], int winner);
+
+#endif
